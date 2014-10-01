9f9cdd9d645d37af866f26982d74eeae80838eab
David Gadd <gaddzeit@yahoo.ca>
2014-10-01 00:14:35 -0700

    Fourth test: any-length comma-delimited array should return sum

--- src/Calculator.js
+++ src/Calculator.js
@@ -6,12 +6,18 @@ var Calculator = {
         return this;
     },
     add: function(numbersToAdd) {
-        if(numbersToAdd.indexOf(",") > -1) {
-            var numbersArray = numbersToAdd.split(",");
-            return parseInt(numbersArray[0]) + parseInt(numbersArray[1]);
-        }
+        if(numbersToAdd.indexOf(",") > -1)
+            return this.splitAndSum(numbersToAdd);
         else if(numbersToAdd.length > 0)
             return parseInt(numbersToAdd);
         return 0;
+    },
+    splitAndSum: function(numbersToAdd) {
+        var numbersArray = numbersToAdd.split(",");
+        var total = 0;
+        for (var i in numbersArray) {
+            total += parseInt(numbersArray[i]);
+        }
+        return total;
     }
 };
